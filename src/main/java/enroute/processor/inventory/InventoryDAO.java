2dd021b30c10f49ab222bd9b07134bb590f99bb7
raptor0719 <shortman_gamer@hotmail.com>
2018-10-15 23:51:51 -0400

    Add skeleton of inventory db access
    - Add InventoryDAO that will interface with the inventory DB
    - Add config file loading

---
+++ src/main/java/enroute/processor/inventory/InventoryDAO.java
@@ -0,0 +1,16 @@
+package enroute.processor.inventory;
+
+import java.util.Collection;
+import java.util.Collections;
+
+// TODO: Need to actually implement this
+public class InventoryDAO {
+
+	public InventoryDAO(final String connectionURI) {
+
+	}
+
+	public Collection<InventoryEntry> getOpenEntries() {
+		return Collections.<InventoryEntry>emptyList();
+	}
+}
--- src/main/java/enroute/processor/main/Main.java
+++ src/main/java/enroute/processor/main/Main.java
@@ -1,8 +1,13 @@
 package enroute.processor.main;
 
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.Properties;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+
+import enroute.processor.inventory.InventoryDAO;
+import enroute.processor.inventory.InventoryEntry;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,8 +15,13 @@ public class Main {
 			return;
 
 		final Logger logger = Logger.getLogger(Main.class.getName());
+		final Properties config = getConfiguration();
 
 		logger.info("Starting EnRoute Inventory Processor...");
+
+		logger.info("Retrieving open inventory");
+		final InventoryDAO inventory = new InventoryDAO(config.getProperty("inventory_db_url"));
+		final Collection<InventoryEntry> openEntries = inventory.getOpenEntries();
 	}
 
 	private static boolean loadLoggerConfiguration() {
@@ -25,5 +35,17 @@ public class Main {
 		}
 
 		return true;
+	}
+
+	private static Properties getConfiguration() {
+		final Properties config = new Properties();
+
+		try {
+			config.load(Main.class.getClassLoader().getResourceAsStream("enroute.config"));
+		} catch (Throwable t) {
+			throw new RuntimeException(t);
+		}
+
+		return config;
 	}
 }
